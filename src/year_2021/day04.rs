e96d1a414acbde55afa948f2f5f8ff7d84f22225
loethor <lrb2392@gmail.com>
2022-11-11 15:00:16 +0100

    Added day 4 part 2 solution

--- src/year_2021/day04.rs
+++ src/year_2021/day04.rs
@@ -5,13 +5,13 @@ pub fn run(input: Vec<String>) {
     println!("part 1: {}", part1);
     assert_eq!(part1, 63552);
 
-    // let part2 = solve_part2(&input);
-    // println!("part 2: {}", part2);
-    // assert_eq!(part2, 793873);
+    let part2 = solve_part2(&input);
+    println!("part 2: {}", part2);
+    assert_eq!(part2, 9020);
 }
 
 fn solve_part1(input: &Vec<String>) -> i32 {
-
+    
     let header = &input[0];
     let mut numbers_drawn:Vec<i32> = Vec::new();
     for number in header.split(','){
@@ -20,8 +20,12 @@ fn solve_part1(input: &Vec<String>) -> i32 {
 
     let mut list_of_boards:Vec<Board> = Vec::new();
 
+    let mut board_id = 0;
     for line in &input[1..]{
-        if line.len() == 0 {list_of_boards.push(Board { columns: Vec::new(), rows: Vec::new() })}
+        if line.len() == 0 {
+            list_of_boards.push(Board { columns: Vec::new(), rows: Vec::new(), id: board_id});
+            board_id += 1;
+        }
     }
 
     let mut current_board = 0;
@@ -34,9 +38,7 @@ fn solve_part1(input: &Vec<String>) -> i32 {
                     if character.is_empty(){continue;}
                     let new_number = Number{value: character.trim().parse().expect("Expected a number."), drawn: false};
                     new_row.add_element(new_number);
-                    
                 }
-                
                 list_of_boards[current_board].rows.push(new_row);
                 if list_of_boards[current_board].rows.len() == 5 {current_board +=1}
             },
@@ -52,7 +54,7 @@ fn solve_part1(input: &Vec<String>) -> i32 {
     let mut winner_number = 0;
     'number_loop: for number in numbers_drawn{
         for board in &mut list_of_boards{
-            if board.check_if_number_in_board(number){
+            if board.check_if_number_produces_bingo(number){
                 println!("{number}");
                 board.print_board();
                 winner_number = number;
@@ -64,13 +66,79 @@ fn solve_part1(input: &Vec<String>) -> i32 {
     board_points * winner_number
 }
 
+fn solve_part2(input: &Vec<String>) -> i32 {
+    let header = &input[0];
+    let mut numbers_drawn:Vec<i32> = Vec::new();
+    for number in header.split(','){
+        numbers_drawn.push(number.parse().expect("Couldn't parse!"));
+    }
+
+    let mut list_of_boards:Vec<Board> = Vec::new();
+
+    let mut board_id = 0;
+    for line in &input[1..]{
+        if line.len() == 0 {
+            list_of_boards.push(Board { columns: Vec::new(), rows: Vec::new(), id: board_id});
+            board_id += 1;
+        }
+    }
+
+    let mut current_board = 0;
+    for line in &input[1..]{
+        match line.len().cmp(&0) {
+            Ordering::Equal => continue,
+            Ordering::Greater => {
+                let mut new_row = Row{elements: Vec::new(), bingo: false};
+                for character in line.split(" "){
+                    if character.is_empty(){continue;}
+                    let new_number = Number{value: character.trim().parse().expect("Expected a number."), drawn: false};
+                    new_row.add_element(new_number);
+                }
+                list_of_boards[current_board].rows.push(new_row);
+                if list_of_boards[current_board].rows.len() == 5 {current_board +=1}
+            },
+            Ordering::Less => panic!("Lines cannot be negative."),   
+        }
+    }
+
+    for board in &mut list_of_boards{
+        board.populate_columns();
+    }
+
+    let number_of_boards = list_of_boards.len();
+
+    let mut board_points = 0;
+    let mut winner_number = 0;
+    let mut list_of_winners:Vec<i32> = Vec::new();
+    'number_loop: for number in numbers_drawn{
+        for board in &mut list_of_boards{
+            if list_of_winners.contains(&board.id){continue;} 
+            if board.check_if_number_produces_bingo(number){
+                if !list_of_winners.contains(&board.id){list_of_winners.push(board.id)}
+                if list_of_winners.len() == number_of_boards{
+                    println!("{number}");
+                    board.print_board();
+                    println!("");
+                    board.print_state();
+                    winner_number = number;
+                    board_points = board.calculate_sum_of_not_drawn_numbers();
+                    break 'number_loop;
+                }
+            }
+        }
+    }    
+    board_points * winner_number
+
+}
+
 struct Board{
     rows:Vec<Row>,
     columns:Vec<Column>,
+    id:i32,
 }
 
 impl Board{
-    fn check_if_number_in_board(&mut self, number: i32) -> bool{
+    fn check_if_number_produces_bingo(&mut self, number: i32) -> bool{
         for row in &mut self.rows{
             for element in &mut row.elements{
                 if number == element.value{element.was_drawn();}
@@ -111,7 +179,7 @@ impl Board{
     
     fn populate_columns(&mut self){
         for _ in 0..5{
-            self.columns.push(Column{elements: Vec::new(), bingo:false})
+            self.columns.push(Column{elements: Vec::new()})
         }
 
         for row in &self.rows{
