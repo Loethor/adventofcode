b38393dfacde576345a56dc38be863b37bf5e22f
loethor <lrb2392@gmail.com>
2022-11-19 17:53:37 +0100

    Added solution for day 10

--- src/main.rs
+++ src/main.rs
@@ -18,15 +18,16 @@ fn main() {
 
     match (year, day) {
         // 2021
-        (2021, 1) => year_2021::day01::run(inputs::read_numbers(year, day)),
-        (2021, 2) => year_2021::day02::run(inputs::read(year, day)),
-        (2021, 3) => year_2021::day03::run(inputs::read(year, day)),
-        (2021, 4) => year_2021::day04::run(inputs::read(year, day)),
-        (2021, 5) => year_2021::day05::run(inputs::read(year, day)),
-        (2021, 6) => year_2021::day06::run(inputs::read(year, day)),
-        (2021, 7) => year_2021::day07::run(inputs::read(year, day)),
-        (2021, 8) => year_2021::day08::run(inputs::read(year, day)),
-        (2021, 9) => year_2021::day09::run(inputs::read(year, day)),
+        (2021,  1) => year_2021::day01::run(inputs::read_numbers(year, day)),
+        (2021,  2) => year_2021::day02::run(inputs::read(year, day)),
+        (2021,  3) => year_2021::day03::run(inputs::read(year, day)),
+        (2021,  4) => year_2021::day04::run(inputs::read(year, day)),
+        (2021,  5) => year_2021::day05::run(inputs::read(year, day)),
+        (2021,  6) => year_2021::day06::run(inputs::read(year, day)),
+        (2021,  7) => year_2021::day07::run(inputs::read(year, day)),
+        (2021,  8) => year_2021::day08::run(inputs::read(year, day)),
+        (2021,  9) => year_2021::day09::run(inputs::read(year, day)),
+        (2021, 10) => year_2021::day10::run(inputs::read(year, day)),
 
 
         (_, _) => panic!("Not implemented :("),
